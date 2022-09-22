f8ccd706806762224d67c02f1a03b61f9148debc
Pedro Viana <pedroviana.developer13@gmail.com>
2022-09-22 18:47:51 -0300

    exercicio agora a pratica fiz ate o 3

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript/dia-2-javascript-array-e-loop-for/exercicios-agora-a-pratica/scripts.js
@@ -0,0 +1,26 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// usado no exercício 2 let result = 0;
+
+// Exercício 1
+// for (let index = 0; index < numbers.length; index += 1) {
+//     console.log(numbers[index]);
+// }
+
+
+// Exercício 2
+// for (let index = 0; index < numbers.length; index += 1) {
+//     result += numbers[index];
+// }
+
+// console.log(result);
+
+// Exercício 3
+// let sum = 0;
+
+// for (let index = 0; index < numbers.length; index +=1) {
+//     sum += numbers[index];
+// }
+
+// let average = sum / numbers.length;
+
+// console.log(average);
