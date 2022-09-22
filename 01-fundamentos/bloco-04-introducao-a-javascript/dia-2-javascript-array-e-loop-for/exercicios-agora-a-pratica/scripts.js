2e63d63c36c1e6d407511fb39d752e08dffa3152
Pedro Viana <pedroviana.developer13@gmail.com>
2022-09-22 19:06:44 -0300

    até o exercício 6

--- 01-fundamentos/bloco-04-introducao-a-javascript/dia-2-javascript-array-e-loop-for/exercicios-agora-a-pratica/scripts.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript/dia-2-javascript-array-e-loop-for/exercicios-agora-a-pratica/scripts.js
@@ -1,5 +1,6 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-// usado no exercício 2 let result = 0;
+// usado no exercício 2 e 4 
+// let result = 0;
 
 // Exercício 1
 // for (let index = 0; index < numbers.length; index += 1) {
@@ -24,3 +25,42 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // let average = sum / numbers.length;
 
 // console.log(average);
+
+// Exercício 4
+// for (let index = 0; index < numbers.length; index += 1) {
+//     result += numbers[index];
+// }
+
+// result = result / numbers.length;
+
+// if (result > 20) {
+//     console.log('Valor maior que 20');
+// } else {
+//     console.log('Valor menor ou igual a 20');
+// }
+
+// Exercício 5 FOGUETE
+// let higherNumber = numbers[0];
+
+// for (let index = 1; index < numbers.length; index += 1) {
+//     if (numbers[index] > higherNumber) {
+//         higherNumber = numbers[index];
+//     }
+// }
+
+// console.log(higherNumber);
+
+// Exercício 6
+// let result = 0;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//     if (numbers[index] % 2 !== 0) {
+//         result += 1;
+//     }
+// }
+
+// if (result === 0) {
+//     console.log('nenhum número ímpar encontrado!');
+// } else {
+//     console.log(result);
+// }
