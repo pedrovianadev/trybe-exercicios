2e3fd839004b559aa0dd4c421054987adefed709
Pedro Viana <pedroviana.developer13@gmail.com>
2022-09-24 19:15:45 -0300

    exercicios bonus 2 e 3 precisa estudar mais sobre eles

--- 01-fundamentos/bloco-04-introducao-a-javascript/bonus.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript/bonus.js
@@ -1,13 +1,43 @@
-let n = 15;
+// Exercício bonus 1
+// let n = 15;
 
-let symbol = '*'
+// let symbol = '*'
 
+// let inputLine = '';
+
+// for (let index = 0; index < n; index += 1){
+//     inputLine = inputLine + symbol;
+// }
+
+// for (let index = 0; index < n; index += 1){
+//     console.log(inputLine);
+// }
+
+// Exercício bonus 2
+// let size = 3;
+// let symbol = '*';
+// let inputLine = '';
+
+// for (let lineIndex = 0; lineIndex <= size; lineIndex +=1) {
+//     console.log(inputLine);
+//     inputLine = inputLine + symbol;
+// }
+
+// Exercício bonus 3
+let size = 5;
+let symbol = '*';
 let inputLine = '';
+let inputPosition = size - 1;
 
-for (let index = 0; index < n; index += 1){
-    inputLine = inputLine + symbol;
-}
-
-for (let index = 0; index < n; index += 1){
+for (let lineIndex = 0; lineIndex < size; lineIndex += 1){
+    for(let columnIndex = 0; columnIndex < size; columnIndex += 1){
+        if (columnIndex < inputPosition){
+            inputLine = inputLine + ' ';
+        } else {
+            inputLine = inputLine +symbol;
+        }
+    }
     console.log(inputLine);
+    inputLine = '';
+    inputPosition -= 1;
 }
