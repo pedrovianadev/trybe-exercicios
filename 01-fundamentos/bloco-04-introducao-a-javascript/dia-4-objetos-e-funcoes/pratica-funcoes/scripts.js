208b8f330b0ca9ccc3ab76e3501167cb9b19aa64
Pedro Viana <pedroviana.developer13@gmail.com>
2022-09-26 16:22:07 -0300

    exercicio 2 do numero maior que o outro

--- 01-fundamentos/bloco-04-introducao-a-javascript/dia-4-objetos-e-funcoes/pratica-funcoes/scripts.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript/dia-4-objetos-e-funcoes/pratica-funcoes/scripts.js
@@ -1,21 +1,31 @@
-function soma (a, b) {
-    return a + b;
+// function soma (a, b) {
+//     return a + b;
+// }
+
+// function sub (a, b) {
+//     return a - b;
+// }
+
+// function mult (a, b) {
+//     return a * b;
+// }
+
+// function div (a, b) {
+//     return a / b;
+// }
+
+// function mod (a, b) {
+//     return a % b;
+// }
+
+// console.log(mod(5, 29));
+
+function biggestNumber (firstNumber, secondNumber) {
+    if (firstNumber > secondNumber) {
+        return firstNumber + ' é maior que ' + secondNumber;
+    } else {
+        return secondNumber + ' é maior que ' + firstNumber;
+    }
 }
 
-function sub (a, b) {
-    return a - b;
-}
-
-function mult (a, b) {
-    return a * b;
-}
-
-function div (a, b) {
-    return a / b;
-}
-
-function mod (a, b) {
-    return a % b;
-}
-
-console.log(mod(5, 29));
+console.log(biggestNumber(101,89));
