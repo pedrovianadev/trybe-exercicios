11216336c3bba26fc5ced303dc4ab4235ca419c7
Pedro Viana <pedroviana.developer13@gmail.com>
2022-09-26 16:18:58 -0300

    exercicio 1 de functions

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript/dia-4-objetos-e-funcoes/pratica-funcoes/scripts.js
@@ -0,0 +1,21 @@
+function soma (a, b) {
+    return a + b;
+}
+
+function sub (a, b) {
+    return a - b;
+}
+
+function mult (a, b) {
+    return a * b;
+}
+
+function div (a, b) {
+    return a / b;
+}
+
+function mod (a, b) {
+    return a % b;
+}
+
+console.log(mod(5, 29));
