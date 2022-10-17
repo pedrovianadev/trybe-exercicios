74816c67dfcb1e236e7298f1d652a5297581997a
Pedro Viana <pedroviana.developer13@gmail.com>
2022-10-17 18:09:47 -0300

    exercício 2 dia 7.1

--- 01-fundamentos/bloco-07-introducao-a-javaScript-ES6-e-testes-unitarios/dia-1-javaScript-ES6-let,const,arrow-functions-e-template-literals/script.js
+++ 01-fundamentos/bloco-07-introducao-a-javaScript-ES6-e-testes-unitarios/dia-1-javaScript-ES6-let,const,arrow-functions-e-template-literals/script.js
@@ -12,3 +12,20 @@ const testingScope = (escopo) => {
   testingScope(true);
 
 //   Exercício 2
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const sortOddsAndEvens = (array) => {
+    for (let index = 1; index < array.length; index += 1) {
+        for (let index2 = 0; index2 < index; index2 += 1) {
+            if (array[index] < array[index2]) {
+                let position = array[index];
+                array[index] = array[index2];
+                array[index2] = position;
+            }
+        }
+    }
+    return array;
+};
+// Seu código aqui.
+
+console.log(sortOddsAndEvens(array)); // será necessário alterar essa linha 😉
