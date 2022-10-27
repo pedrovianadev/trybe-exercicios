bdf7c5051d280c10be9ac83fdfcf0b9dc4ff3072
Pedro Viana <pedroviana.developer13@gmail.com>
2022-10-27 14:16:57 -0300

    exercicio da higher order functions na branch errada

---
+++ 01-fundamentos/bloco-08-higher-order-functions-do-JavaScript-ES6/dia-1-JavaScript-ES6-Higher-Order-Functions-map-e-filter/higher-order-functions/script.js
@@ -0,0 +1,12 @@
+const wakeUp = () => 'Acordando!!';
+const breakfast = () => 'Bora tomar café!!';
+const sleep = () => 'Partiu dormir!!';
+
+const doingThings = (callback) => {
+    const result = callback();
+    console.log(result);
+};
+
+doingThings(wakeUp);
+doingThings(breakfast);
+doingThings(sleep);
